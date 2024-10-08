7a5248cb6c814b1b04ead32ca33b502c59d953d3
rubenzhu <rubenzhu@hotmail.com>
2024-10-08 15:14:23 +0200

    change vector<shared_ptr<Predicate>> to vector<Predicate>

--- model/Rule.hh
+++ model/Rule.hh
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <vector>
-#include <memory>
 
 #include "Predicate.hh"
 
@@ -9,14 +8,14 @@ class Rule {
 
 	private:
 		std::string 					_name;
-		std::vector<std::shared_ptr<Predicate>> 	_premise;
-		std::vector<std::shared_ptr<Predicate>> 	_consequence;
+		std::vector<Predicate> 	_premise;
+		std::vector<Predicate> 	_consequence;
 
 	public:
 		Rule
 		(std::string name, 
-		 std::vector<std::shared_ptr<Predicate>> premise, 
-		 std::vector<std::shared_ptr<Predicate>> consequence)
+		 std::vector<Predicate> premise, 
+		 std::vector<Predicate> consequence)
 			: _name(name), _premise(premise), _consequence(consequence) {}
 
 
