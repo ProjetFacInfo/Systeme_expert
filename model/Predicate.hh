27c23be040d26430eace6454eb60a656d2923ffd
rubenzhu <rubenzhu@hotmail.com>
2024-10-08 15:22:02 +0200

    Parameter -> vector<Parameter>

--- model/Predicate.hh
+++ model/Predicate.hh
@@ -1,15 +1,16 @@
 #include <string>
+#include <vector>
 
 #include "Parameter.hh"
 
 class Predicate {
 
 	private:
-		std::string 	_name;
-		Parameter 	_parameter;
-		bool 		_value;
+		std::string 		_name;
+		std::vector<Parameter> 	_parameter;
+		bool 			_value;
 	
 	public:
-		Predicate(std::string name, Parameter parameter, bool value) 
+		Predicate(std::string name, std::vector<Parameter> parameter, bool value) 
 			: _name(name), _parameter(parameter), _value(value) {}
 };
