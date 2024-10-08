1add7f52c533cf9bcaf6f55004d4061236a2471d
etud user <etud@localhost>
2024-10-08 14:24:28 +0200

    ajout class Fact

--- model/Fact.hh
+++ model/Fact.hh
@@ -1,5 +1,21 @@
 #pragma once
 
+#include <string>
+#include <vector>
+
+#include "Parameter.hh"
+
 class Fact {
 
+private:
+
+    std::string _name;
+    std::vector<Parameter> _parameters;
+
+public:
+
+    Fact(std::string name, std::vector<Parameter> parameters):_name(name),_parameters(parameters){}
+
 };
+
+   
